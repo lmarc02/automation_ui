0de94c179b3b074c514b850dabd6037dde8e2fbb
lore <lore@test.com>
2019-07-27 21:21:28 +0300

    login test updates

--- Automation3/src/test/LogInTest.java
+++ Automation3/src/test/LogInTest.java
@@ -1,8 +1,10 @@
 
+import jdk.internal.jline.internal.TestAccessible;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pages.LogInPage;
+import sun.jvm.hotspot.utilities.Assert;
 
 public class LogInTest extends BaseTest {
     private LogInPage logInPage = new LogInPage(driver);
@@ -20,6 +22,20 @@ public class LogInTest extends BaseTest {
         Assert.assertEquals(logInPage.isCartDisplayed(), true);
         Assert.assertEquals(logInPage.getHeadingText(), "AUTHENTICATION",
                 "Incorrect heading text on Authentication page.");
+        Assert.assertEquals(logInPage.isHeadingTextDisplayed(), true,
+                "Authentication heading text is not displayed.");
+        Assert.assertEquals(logInPage.isHeadingTextDisplayed(), true,
+                "Authentication heading text is not displayed.");
+    }
+    @Test
+    public void checkLoginPages(){
+        Assert.assertEquals(logInPage.getCreateAnAccountBtnText(), "Create an account",
+                "Incorrect text on Create an account button.");
+        Assert.assertEquals(logInPage.isCartDisplayed(), true);
+        Assert.assertEquals(logInPage.getHeadingText(), "AUTHENTICATION",
+                "Incorrect heading text on Authentication page.");
+        Assert.assertEquals(logInPage.isHeadingTextDisplayed(), true,
+                "Authentication heading text is not displayed.");
         Assert.assertEquals(logInPage.isHeadingTextDisplayed(), true,
                 "Authentication heading text is not displayed.");
     }
